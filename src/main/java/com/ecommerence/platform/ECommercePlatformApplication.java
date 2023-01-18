ac92a4787d0fa482a886c6c4ed0cf356f76a3e81
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-01-18 10:18:29 +0200

    init spring boot project with spring-data-jpa and oracle db driver

---
+++ src/main/java/com/ecommerence/platform/ECommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerence.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerence/platform/ECommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerence.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
