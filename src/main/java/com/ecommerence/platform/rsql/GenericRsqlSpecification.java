648ff6cb07d427bda606c18b4820d70021ef08e8
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-02-14 15:04:04 +0200

    rsql fix inner object problem

--- src/main/java/com/ecommerence/platform/rsql/GenericRsqlSpecification.java
+++ src/main/java/com/ecommerence/platform/rsql/GenericRsqlSpecification.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,66 +20,79 @@ public class GenericRsqlSpecification<T> implements Specification<T> {
     private List<String> arguments;
 
     @Override
-    public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
-        List<Object> args = castArguments(root);
+    public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query,
+                                 CriteriaBuilder builder) {
+        Path<String> propertyExpression = parseProperty(root);
+        List<Object> args = castArguments(propertyExpression);
         Object argument = args.get(0);
         switch (RsqlSearchOperation.getSimpleOperator(operator)) {
+            case EQUAL:
+                if (argument instanceof String)
+                    return builder.like(propertyExpression,
+                            argument.toString().replace('*', '%'));
+                else if (argument == null)
+                    return builder.isNull(propertyExpression);
+                else return builder.equal(propertyExpression, argument);
 
-            case EQUAL: {
-                if (argument instanceof String) {
-                    return builder.like(root.get(property), argument.toString().replace('*', '%'));
-                } else if (argument == null) {
-                    return builder.isNull(root.get(property));
-                } else {
-                    return builder.equal(root.get(property), argument);
-                }
-            }
-            case NOT_EQUAL: {
-                if (argument instanceof String) {
-                    return builder.notLike(root.<String> get(property), argument.toString().replace('*', '%'));
-                } else if (argument == null) {
-                    return builder.isNotNull(root.get(property));
-                } else {
-                    return builder.notEqual(root.get(property), argument);
-                }
-            }
-            case GREATER_THAN: {
-                return builder.greaterThan(root.<String> get(property), argument.toString());
-            }
-            case GREATER_THAN_OR_EQUAL: {
-                return builder.greaterThanOrEqualTo(root.<String> get(property), argument.toString());
-            }
-            case LESS_THAN: {
-                return builder.lessThan(root.<String> get(property), argument.toString());
-            }
-            case LESS_THAN_OR_EQUAL: {
-                return builder.lessThanOrEqualTo(root.<String> get(property), argument.toString());
-            }
+            case NOT_EQUAL:
+                if (argument instanceof String)
+                    return builder.notLike(propertyExpression,
+                            argument.toString().replace('*', '%'));
+                else if (argument == null)
+                    return builder.isNotNull(propertyExpression);
+                else return builder.notEqual(propertyExpression, argument);
+
+            case GREATER_THAN:
+                return builder.greaterThan(propertyExpression,
+                        argument.toString());
+
+            case GREATER_THAN_OR_EQUAL:
+                return builder.greaterThanOrEqualTo(propertyExpression,
+                        argument.toString());
+
+            case LESS_THAN:
+                return builder.lessThan(propertyExpression,
+                        argument.toString());
+
+            case LESS_THAN_OR_EQUAL:
+                return builder.lessThanOrEqualTo(propertyExpression,
+                        argument.toString());
             case IN:
-                return root.get(property).in(args);
+                return propertyExpression.in(args);
             case NOT_IN:
-                return builder.not(root.get(property).in(args));
+                return builder.not(propertyExpression.in(args));
         }
 
         return null;
     }
 
-    private List<Object> castArguments(final Root<T> root) {
+    // This method will help us diving deep into nested property using the dot convention
+    // The originial tutorial did not have this, so it can only parse the shallow properties.
+    private Path<String> parseProperty(Root<T> root) {
+        Path<String> path;
+        if (property.contains(".")) {
+            // Nested properties
+            String[] pathSteps = property.split("\\.");
+            String step = pathSteps[0];
+            path = root.get(step);
 
-        Class<? extends Object> type = root.get(property).getJavaType();
-
-        List<Object> args = arguments.stream().map(arg -> {
-            if (type.equals(Integer.class)) {
-                return Integer.parseInt(arg);
-            } else if (type.equals(Long.class)) {
-                return Long.parseLong(arg);
-            } else {
-                return arg;
+            for (int i = 1; i <= pathSteps.length - 1; i++) {
+                path = path.get(pathSteps[i]);
             }
-        }).collect(Collectors.toList());
-
-        return args;
+        } else {
+            path = root.get(property);
+        }
+        return path;
     }
 
+    private List<Object> castArguments(Path<?> propertyExpression) {
+        Class<?> type = propertyExpression.getJavaType();
 
+        return arguments.stream().map(arg -> {
+            if (type.equals(Integer.class)) return Integer.parseInt(arg);
+            else if (type.equals(Long.class)) return Long.parseLong(arg);
+            else if (type.equals(Byte.class)) return Byte.parseByte(arg);
+            else return arg;
+        }).collect(Collectors.toList());
+    }
 }
